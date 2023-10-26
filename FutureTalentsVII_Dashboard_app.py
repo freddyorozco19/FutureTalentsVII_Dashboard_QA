e987eb84d3cc9d8fc555c1b8801b39db2c8753ae
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2023-10-26 01:12:32 -0500

    Update FutureTalentsVII_Dashboard_app.py

--- FutureTalentsVII_Dashboard_app.py
+++ FutureTalentsVII_Dashboard_app.py
@@ -728,12 +728,12 @@ if selected == "Rankings":
             dftransp = event_counts22.transpose()            
             dftransp = dftransp.reset_index()            
             metricsFK = list(dftransp['index'].drop_duplicates())
-            #metricsFK = metricsFK[15:]
+            metricsFK = metricsFK[6:]
             metselFK = st.selectbox('Selecciona métrica uno:', metricsFK)
         with fk02:
             #SELECT METRIC TWO
             metricsFK2 = list(dftransp['index'].drop_duplicates())
-            #metricsFK2 = metricsFK2[16:]
+            metricsFK2 = metricsFK2[6:]
             metselFK2 = st.selectbox('Selecciona métrica dos:', metricsFK2)
         with fk03:
             #SELECT POSITION OPTION
@@ -748,8 +748,8 @@ if selected == "Rankings":
             event_counts22 = event_counts22[event_counts22['Position'].isin(posselFK)]
         submit_buttonFK = st.form_submit_button(label='Aceptar') 
     c = alt.Chart(event_counts22, width=800, height=400).mark_circle().encode(
-          x='Total Passes', y='Total Progressive Passes', size='Minutes Played', color='Minutes Played', 
-          tooltip=['PlayerID', 'Total Passes', 'Total Progressive Passes', 'Minutes Played'] # <--- tooltip part
+          x=metselFK, y=metselFK2, size='Minutes Played', color='Position', 
+          tooltip=['PlayerID', metselFK, metselFK2, 'Minutes Played'] # <--- tooltip part
       )
     
     st.altair_chart(c, theme="streamlit")
